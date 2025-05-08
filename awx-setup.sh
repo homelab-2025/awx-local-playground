f655c97de1fdecd8055871f197c4a31328a249cb
Enzo G <gl@enzog.mozmail.com>
2025-05-08 23:53:40 +0200

    add awx setup script

---
+++ awx-setup.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+set -e
+
+# Set variables
+REPO_URL="git@github.com:ansible/awx-operator.git"
+TAG="2.7.2"  # Change this to the desired tag
+NAMESPACE="awx"
+AWX_NAME="awx-demo"
+
+param=$1
+
+case $param in
+	deploy)
+		echo "[*] Clean up if awx repository already cloned"
+		[ -d "awx-operator" ] && rm -rf awx-operator
+
+		echo "[*] Cloning AWX Operator repository..."
+		git clone $REPO_URL
+		cd awx-operator || exit
+		git checkout tags/$TAG
+
+		echo "[*] Setting up namespace..."
+		kubectl create namespace $NAMESPACE --dry-run=client -o yaml | kubectl apply -f -
+		kubectl config set-context --current --namespace=$NAMESPACE
+
+		echo "[*] Creating AWX manifests..."
+cat <<EOF > kustomization.yml
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+resources:
+  - github.com/ansible/awx-operator/config/default?ref=$TAG
+  - $AWX_NAME.yml
+images:
+  - name: quay.io/ansible/awx-operator
+    newTag: $TAG
+namespace: $NAMESPACE
+EOF
+
+		cat <<EOF > $AWX_NAME.yml
+---
+apiVersion: awx.ansible.com/v1beta1
+kind: AWX
+metadata:
+  name: $AWX_NAME
+spec:
+  service_type: nodeport
+EOF
+
+		echo "[*] Applying manifests..."
+		kubectl apply -k .
+		;;
+	pods-status)
+		watch kubectl get all
+		;;
+	awx-status)
+		kubectl logs -f deployments/awx-operator-controller-manager -c awx-manager
+		;;
+	get-vars)
+		echo "[*] Retrieving admin password..."
+		ADMIN_PASSWORD=$(kubectl get secret ${AWX_NAME}-admin-password -o jsonpath="{.data.password}" | base64 --decode)
+		echo "AWX Login: admin"
+		echo "AWX Password: $ADMIN_PASSWORD"
+
+		echo "[*] Getting NodePort for AWX..."
+		NODE_PORT=$(kubectl get svc -l "app.kubernetes.io/managed-by=awx-operator" -o jsonpath="{.items[?(@.metadata.name=='${AWX_NAME}-service')].spec.ports[0].nodePort}")
+		echo "AWX is accessible at: http://localhost:$NODE_PORT/"
+		;;
+	*)
+		echo "Usage: $0 {deploy|pods-status|awx-status|get-vars}"
+		exit 1
+		;;
+esac
